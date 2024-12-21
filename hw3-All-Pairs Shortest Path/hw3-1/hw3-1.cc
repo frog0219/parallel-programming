549d501d5de430f15810c3d81d3c8a7a69361cd5
frog910219 <frog910219@gmail.com>
2024-12-21 18:15:57 +0800

    hw1

--- hw3-All-Pairs Shortest Path/hw3-1/hw3-1.cc
+++ hw3-All-Pairs Shortest Path/hw3-1/hw3-1.cc
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
 const int INF = ((1 << 30) - 1);
 const int V = 50010;
+
 void input(char* inFileName);
 void output(char* outFileName);
-
-void block_FW(int B);
-int ceil(int a, int b);
-void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height);
 
 int n, m;
 static int Dist[V][V];
 
 int main(int argc, char* argv[]) {
     input(argv[1]);
-    int B = 512;
-    block_FW(B);
-    for(int i = 0;i < n ; i++){
-        for(int j = 0 ; j < n ;j++) {
-            if(Dist[i][j] == INF) printf("INF ");
-            else printf("%d " ,Dist[i][j]);
+
+    #pragma omp parallel
+    for (int k = 0; k < n; ++k) {
+        #pragma omp for schedule(dynamic)
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (Dist[i][k] < INF && Dist[k][j] < INF) {
+                    Dist[i][j] = min(Dist[i][j], Dist[i][k] + Dist[k][j]);
+                }
+            }
         }
-        printf("\n");
     }
+
     output(argv[2]);
     return 0;
 }
@@ -60,60 +63,4 @@ void output(char* outFileName) {
         fwrite(Dist[i], sizeof(int), n, outfile);
     }
     fclose(outfile);
-}
-
-int ceil(int a, int b) { return (a + b - 1) / b; }
-
-void block_FW(int B) {
-    int round = ceil(n, B);
-    for (int r = 0; r < round; ++r) {
-        printf("%d %d\n", r, round);
-        fflush(stdout);
-        /* Phase 1*/
-        cal(B, r, r, r, 1, 1);
-
-        /* Phase 2*/
-        cal(B, r, r, 0, r, 1);
-        cal(B, r, r, r + 1, round - r - 1, 1);
-        cal(B, r, 0, r, 1, r);
-        cal(B, r, r + 1, r, 1, round - r - 1);
-
-        /* Phase 3*/
-        cal(B, r, 0, 0, r, r);
-        cal(B, r, 0, r + 1, round - r - 1, r);
-        cal(B, r, r + 1, 0, r, round - r - 1);
-        cal(B, r, r + 1, r + 1, round - r - 1, round - r - 1);
-    }
-}
-
-void cal(
-    int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height) {
-    int block_end_x = block_start_x + block_height;
-    int block_end_y = block_start_y + block_width;
-    #pragma omp parallel for collapse(2) schedule(dynamic)
-    for (int b_i = block_start_x; b_i < block_end_x; ++b_i) {
-        for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
-            // To calculate B*B elements in the block (b_i, b_j)
-            // For each block, it need to compute B times
-            for (int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
-                // To calculate original index of elements in the block (b_i, b_j)
-                // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
-                int block_internal_start_x = b_i * B;
-                int block_internal_end_x = (b_i + 1) * B;
-                int block_internal_start_y = b_j * B;
-                int block_internal_end_y = (b_j + 1) * B;
-
-                if (block_internal_end_x > n) block_internal_end_x = n;
-                if (block_internal_end_y > n) block_internal_end_y = n;
-
-                for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
-                    for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
-                        if (Dist[i][k] + Dist[k][j] < Dist[i][j]) {
-                            Dist[i][j] = Dist[i][k] + Dist[k][j];
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
