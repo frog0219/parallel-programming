aed55ed562d691449c8991e15aecb328483b5f55
frog910219 <frog910219@gmail.com>
2024-12-23 01:05:22 +0800

    hw3_report

--- hw4-FlashAttention/hw4.cu
+++ hw4-FlashAttention/hw4.cu
@@ -10,7 +10,7 @@ void input(char *input_filename);
 void output(char *output_filename);
 void flash_attention(float *q, float *k, float *v, float *o);
 
-__global__ void flash_attention_kenel(float *q, float *k, float *m, float *l, float *o, float *v, int d, float scalar);
+__global__ void flash_attention_kenel(float *q, float *k, float *m, float *l, float *o, float *v, int d, float scalar, int grid_size);
 __device__ void QKDotAndScalar(float *out, float *q, float *k, int d, float scalar);
 __device__ void RowMax(float *out, float *in);
 __device__ void MinusMaxAndExp(float *out, float *in, float *mx );
@@ -27,7 +27,8 @@ double getTimeStamp() {
 
 int B, N, d;
 float *Q, *K, *V, *O;
-
+float *device_q, *device_k, *device_v, *device_o;
+float *device_l, *device_m;
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Usage: %s <input_filename> <output_filename>\n", argv[0]);
@@ -38,6 +39,13 @@ int main(int argc, char *argv[]) {
 
     double start, end;
     start = getTimeStamp();
+
+    cudaMalloc(&device_q, N * d * sizeof(float));
+    cudaMalloc(&device_k, N * d * sizeof(float));
+    cudaMalloc(&device_v, N * d * sizeof(float));
+    cudaMalloc(&device_o, N * d * sizeof(float));
+    cudaMalloc(&device_l, N * sizeof(float));
+    cudaMalloc(&device_m, N * sizeof(float));
 
     for (int i = 0; i < B; i++) {
         flash_attention(
@@ -53,7 +61,12 @@ int main(int argc, char *argv[]) {
     printf("Time: %.3f seconds\n", end - start);
 
     output(argv[2]);
-
+    // cudaFree(device_q);
+    // cudaFree(device_k);
+    // cudaFree(device_v);
+    // cudaFree(device_o);
+    // cudaFree(device_l);
+    // cudaFree(device_m);
     return 0;
 }
 
@@ -93,8 +106,8 @@ void output(char *output_filename) {
 }
 
 void flash_attention(float *q, float *k, float *v, float *o) {
-    float *device_q, *device_k, *device_v, *device_o;
-    float *device_l, *device_m;
+    // float *device_q, *device_k, *device_v, *device_o;
+    // float *device_l, *device_m;
     float *l = (float *)malloc(N * sizeof(float));
     float *m = (float *)malloc(N * sizeof(float));
 
@@ -103,12 +116,12 @@ void flash_attention(float *q, float *k, float *v, float *o) {
         m[i] = FLT_MIN;
     }
 
-    cudaMalloc(&device_q, N * d * sizeof(float));
-    cudaMalloc(&device_k, N * d * sizeof(float));
-    cudaMalloc(&device_v, N * d * sizeof(float));
-    cudaMalloc(&device_o, N * d * sizeof(float));
-    cudaMalloc(&device_l, N * sizeof(float));
-    cudaMalloc(&device_m, N * sizeof(float));
+    // cudaMalloc(&device_q, N * d * sizeof(float));
+    // cudaMalloc(&device_k, N * d * sizeof(float));
+    // cudaMalloc(&device_v, N * d * sizeof(float));
+    // cudaMalloc(&device_o, N * d * sizeof(float));
+    // cudaMalloc(&device_l, N * sizeof(float));
+    // cudaMalloc(&device_m, N * sizeof(float));
 
 
     cudaMemcpy(device_q, q, N * d * sizeof(float), cudaMemcpyHostToDevice);
@@ -121,25 +134,18 @@ void flash_attention(float *q, float *k, float *v, float *o) {
     int grid_size = N / BLOCK_SIZE;
     dim3 block(BLOCK_SIZE, BLOCK_SIZE);
     dim3 grid(grid_size);
-    for (int j = 0; j < grid_size; j++) {
-        flash_attention_kenel<<< grid, block >>>(device_q, device_k + j * BLOCK_SIZE * d, device_m, device_l , device_o, device_v + j * BLOCK_SIZE * d, d , 1.0 / sqrt(d));
-    }
+   
+    flash_attention_kenel<<< grid, block >>>(device_q, device_k, device_m, device_l , device_o, device_v, d , 1.0 / sqrt(d) , grid_size);
+    
     cudaMemcpy(o, device_o, N * d * sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaFree(device_q);
-    cudaFree(device_k);
-    cudaFree(device_v);
-    cudaFree(device_o);
-    cudaFree(device_l);
-    cudaFree(device_m);
 }
-__global__ void flash_attention_kenel(float *q, float *k, float *m, float *l, float *o, float *v, int d, float scalar){
-    __shared__ float shared_q[BLOCK_SIZE * BLOCK_SIZE * 2];
-    __shared__ float shared_k[BLOCK_SIZE * BLOCK_SIZE * 2];
-    __shared__ float shared_v[BLOCK_SIZE * BLOCK_SIZE * 2];
+__global__ void flash_attention_kenel(float *q, float *k, float *m, float *l, float *o, float *v, int d, float scalar , int grid_size){
+    __shared__ float shared_q[BLOCK_SIZE * 64];
+    __shared__ float shared_k[BLOCK_SIZE * 64];
+    __shared__ float shared_v[BLOCK_SIZE * 64];
     __shared__ float shared_m[BLOCK_SIZE];
     __shared__ float shared_l[BLOCK_SIZE];
-    
+    __shared__ float shared_o[BLOCK_SIZE * 64]; 
     __shared__ float sij[BLOCK_SIZE * BLOCK_SIZE];
     __shared__ float mij[BLOCK_SIZE];
     __shared__ float pij[BLOCK_SIZE * BLOCK_SIZE];
@@ -151,27 +157,45 @@ __global__ void flash_attention_kenel(float *q, float *k, float *m, float *l, fl
     int step = d / BLOCK_SIZE;
 
     for(int i = 0 ; i < step; i++){
-        shared_q[ty * d + (i * BLOCK_SIZE + tx)] = q[(start * BLOCK_SIZE + ty)*d + (i * BLOCK_SIZE + tx)];
-        shared_k[tx * d + (i * BLOCK_SIZE + ty)] = k[tx * d + (i * BLOCK_SIZE + ty)];
-        shared_v[tx * d + (i * BLOCK_SIZE + ty)] = v[tx * d + (i * BLOCK_SIZE + ty)];
+        shared_q[ty * d + (i * BLOCK_SIZE + tx)] = q[(start * BLOCK_SIZE + ty) * d + (i * BLOCK_SIZE + tx)];
+        shared_o[ty * d + (i * BLOCK_SIZE + tx)] = o[(start * BLOCK_SIZE + ty) * d + (i * BLOCK_SIZE + tx)];
+        // shared_k[tx * d + (i * BLOCK_SIZE + ty)] = k[tx * d + (i * BLOCK_SIZE + ty) + j * BLOCK_SIZE * d];
+        // shared_v[tx * d + (i * BLOCK_SIZE + ty)] = v[tx * d + (i * BLOCK_SIZE + ty) + j * BLOCK_SIZE * d];
     }
     shared_m[ty] = m[ty + start * BLOCK_SIZE];
     shared_l[ty] = l[ty + start * BLOCK_SIZE];
-    __syncthreads();
 
-    QKDotAndScalar(sij , shared_q , shared_k , d , scalar);
-    __syncthreads();
-    RowMax(mij , sij);
-    __syncthreads();
-    MinusMaxAndExp(pij , sij , mij);
-    __syncthreads();
-    RowSum(lij , pij);
-    __syncthreads();
-    UpdateMiLiOi(shared_m, shared_l, o, mij, lij, pij, shared_v, d);
-    __syncthreads();
+    for (int j = 0; j < grid_size; j++) {
 
-    m[ty + start * BLOCK_SIZE] = shared_m[ty];
-    l[ty + start * BLOCK_SIZE] = shared_l[ty];
+        for(int i = 0 ; i < step; i++){
+            shared_k[tx * d + (i * BLOCK_SIZE + ty)] = k[tx * d + (i * BLOCK_SIZE + ty) + j * BLOCK_SIZE * d];
+            shared_v[tx * d + (i * BLOCK_SIZE + ty)] = v[tx * d + (i * BLOCK_SIZE + ty) + j * BLOCK_SIZE * d];
+        }
+         __syncthreads();
+
+        QKDotAndScalar(sij , shared_q , shared_k , d , scalar);
+
+        __syncthreads();
+
+        RowMax(mij , sij);
+
+        __syncthreads();
+
+        MinusMaxAndExp(pij , sij , mij);
+
+        __syncthreads();
+
+        RowSum(lij , pij);
+
+        __syncthreads();
+        
+        UpdateMiLiOi(shared_m, shared_l, shared_o, mij, lij, pij, shared_v, d);
+    }
+    for(int i = 0 ; i < step; i++){
+        o[(start * BLOCK_SIZE + ty) * d + (i * BLOCK_SIZE + tx)] =  shared_o[ty * d + (i * BLOCK_SIZE + tx)];
+    }
+    // m[ty + start * BLOCK_SIZE] = shared_m[ty];
+    // l[ty + start * BLOCK_SIZE] = shared_l[ty];
 }
 __device__ void QKDotAndScalar(float *out, float *q, float *k, int d, float scalar) {
     int tx = threadIdx.x;
@@ -195,7 +219,6 @@ __device__ void RowMax(float *out, float *in) {
         }
         out[ty] = result;
     }
-    __syncthreads();
 }
 
 __device__ void MinusMaxAndExp(float *out, float *in, float *mx ) {
@@ -214,14 +237,12 @@ __device__ void RowSum(float *out, float *in) {
         }
         out[ty] = result;
     }
-    __syncthreads();
 }
 
-__device__ void UpdateMiLiOi(float *shared_m, float *shared_l, float *o, float *mij, float *lij, float *pij, float *shared_v , int d) {
+__device__ void UpdateMiLiOi(float *shared_m, float *shared_l, float *shared_o, float *mij, float *lij, float *pij, float *shared_v , int d) {
     int tx = threadIdx.x;
     int ty = threadIdx.y;
    
-    int start = blockIdx.x;
     __shared__ float new_m[BLOCK_SIZE];
     __shared__ float new_l[BLOCK_SIZE];
 
@@ -241,7 +262,7 @@ __device__ void UpdateMiLiOi(float *shared_m, float *shared_l, float *o, float *
         for (int t = 0; t < BLOCK_SIZE; t++) {
             pv += pij[ty * BLOCK_SIZE + t] * shared_v[t * d + (j * BLOCK_SIZE + tx)];
         }
-        o[(ty + start * BLOCK_SIZE) * d + (j * BLOCK_SIZE + tx)] = (thread_l * exp(thread_m - new_m[ty]) * o[(ty + start * BLOCK_SIZE) * d + (j * BLOCK_SIZE + tx)] + exp(thread_mij - new_m[ty]) * pv) / new_l[ty];
+        shared_o[ty * d + (j * BLOCK_SIZE + tx)] = (thread_l * exp(thread_m - new_m[ty]) * shared_o[ty * d + (j * BLOCK_SIZE + tx)] + exp(thread_mij - new_m[ty]) * pv) / new_l[ty];
     }
 
     if(tx == 0){
@@ -249,5 +270,4 @@ __device__ void UpdateMiLiOi(float *shared_m, float *shared_l, float *o, float *
         shared_l[ty] = new_l[ty];
     }
 
-    __syncthreads();
 }
